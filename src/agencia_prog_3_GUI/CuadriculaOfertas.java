2f6c485f005b9e6e41fa58401c81d85a1c4c0248
Zaira Diez Garcia <zaira.diez@opendeusto.es>
2025-10-31 09:28:28 +0100

    Vision principal de la pantalla de ofertas

--- src/agencia_prog_3_GUI/CuadriculaOfertas.java
+++ src/agencia_prog_3_GUI/CuadriculaOfertas.java
@@ -1,5 +1,64 @@
 package agencia_prog_3_GUI;
 
-public class CuadriculaOfertas {
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.GridLayout;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+
+public class CuadriculaOfertas extends JFrame{
+	private JTextField oferta;
+	
+	
+	public CuadriculaOfertas() {
+		JPanel mainpanel = new JPanel();
+		JPanel panel1 = new JPanel(new GridLayout(3, 3, 20, 20));
+		for (int i = 0; i < 9; i++) {
+			JButton boton = new JButton("Oferta " + (i + 1));
+			boton.setPreferredSize(new Dimension(200, 120));
+//			boton.setVerticalTextPosition(SwingConstants.TOP);
+//			boton.setHorizontalTextPosition(SwingConstants.CENTER);
+			panel1.add(boton);			
+		}
+		
+	
+		
+		mainpanel.add(panel1);
+		add(mainpanel);
+		
+		
+		setTitle("OFERTAS RECIENTES");
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setSize(800, 500);
+		setMaximumSize(getToolkit().getScreenSize());
+		setResizable(false);
+		setLocationRelativeTo(null);
+				
+		
+		oferta = new JTextField();
+		oferta.setText("OFERTAS RECIENTES");
+		oferta.setEditable(false);
+		oferta.setHorizontalAlignment(JTextField.CENTER);
+		oferta.setFont(new Font("Times new Roman", Font.BOLD, 25));
+		add(oferta, BorderLayout.NORTH);
+		
+	}
+	
+	
+	
+	
+	
+	public static void main(String[] args) {
+		CuadriculaOfertas ofertas = new CuadriculaOfertas();
+		ofertas.setVisible(true);
+	}
+	
+	
 }
