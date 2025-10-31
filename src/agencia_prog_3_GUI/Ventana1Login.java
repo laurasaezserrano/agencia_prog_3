157bb16baea78db07dc45177ccbb2b2fd6de8158
Laura Saez Serrano <laura.saez.serrano@opendeusto.es>
2025-10-31 11:15:19 +0100

    Eliminacion CSV, reajuste de vuelos

--- src/agencia_prog_3_GUI/Ventana1Login.java
+++ src/agencia_prog_3_GUI/Ventana1Login.java
@@ -92,7 +92,7 @@ public class Ventana1Login extends JFrame {
         }
 
         if (validUsers.containsKey(user) && validUsers.get(user).equals(pass)) {
-            statusLabel.setText("✅ Bienvenido, " + user + "!");
+            statusLabel.setText("Bienvenido, " + user + "!"); //Parte inferior del Layout
             JOptionPane.showMessageDialog(this, "Inicio de sesión correcto.", "Éxito", JOptionPane.INFORMATION_MESSAGE);
         } else if (!validUsers.containsKey(user)) {
             int option = JOptionPane.showConfirmDialog(this, "El usuario no está registrado. ¿Deseas registrarte?",
