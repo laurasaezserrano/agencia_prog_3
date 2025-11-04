c77f4fb22d01a3548db5e92d3b34078e4a23cd05
Laura Saez Serrano <laura.saez.serrano@opendeusto.es>
2025-11-04 23:09:43 +0100

    Creacion de clase NECESARIA no creada

---
+++ src/agencia_prog_3_GUI/VentanaUser.java
@@ -0,0 +1,10 @@
+package agencia_prog_3_GUI;
+
+public class VentanaUser {
+//PERFIL
+	
+ // Ventana donde el usuario vea su informacion
+	//OPCION A MODIFICAR PASSWORD
+	// añadir una foto generica user
+	// y que introduzca datos personales que despues se relacionaran con su incio para las reservas
+}
