53703bb96c7c7de47315f1fa1055a74d6ec78deb
Zaira Diez Garcia <zaira.diez@opendeusto.es>
2025-11-05 10:33:12 +0100

    Continuacion de la ventana de usuario

--- src/agencia_prog_3_GUI/VentanaUser.java
+++ src/agencia_prog_3_GUI/VentanaUser.java
@@ -1,6 +1,36 @@
 package agencia_prog_3_GUI;
 
-public class VentanaUser {
+import java.awt.BorderLayout;
+import java.awt.Font;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+public class VentanaUser extends JFrame{
+	private JTextField titulo;
+	private JTextField nombre;
+	private JTextField dni;
+	private JTextField email;
+	private JTextField telefono;
+	private JTextField direccion;
+	
+	public VentanaUser() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setLayout(new BorderLayout(10, 10));
+		setResizable(false);
+		
+		//Titulo
+		titulo = new JTextField("Perfil Usuario");
+		titulo.setEditable(false);
+		titulo.setFont(new Font("Times new Roman", 13, Font.BOLD));
+		titulo.setHorizontalAlignment(JTextField.CENTER);
+		add(titulo, BorderLayout.NORTH);
+		
+		
+		
+		
+	}
 //PERFIL
 	
  // Ventana donde el usuario vea su informacion
