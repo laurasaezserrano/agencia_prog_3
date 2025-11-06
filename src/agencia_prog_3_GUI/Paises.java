c2240f18a4c1f30bd2058226a1cb031740145d09
Zaira Diez Garcia <zaira.diez@opendeusto.es>
2025-11-06 11:05:10 +0100

    Creacion del enum paises

---
+++ src/agencia_prog_3_GUI/Paises.java
@@ -0,0 +1,32 @@
+package agencia_prog_3_GUI;
+
+import java.io.Serializable;
+
+public enum Paises implements Serializable{
+	ES("España"),
+	GB("Reino Unido"),
+	US("Estados Unidos"),
+	TH("Tailandia"),
+	NO("Noruega"),
+	NL("Paises Bajos"),
+	MX("Mexico"),
+	JP("Japon"),
+	IT("Italia"),
+	CH("Suiza"),
+	CO("Colombia"),
+	FR("France"),
+	CA("Canada"),
+	BE("Belgica"),
+	IE("Irlanda");
+	
+	private String nombre;
+
+	private Paises(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getNombre() {
+		return this.nombre;
+	}
+	
+}
