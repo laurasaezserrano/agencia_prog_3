817d60d980751856a4eeaf359ca92d47c68e79e9
Zaira Diez Garcia <zaira.diez@opendeusto.es>
2025-11-08 22:47:42 +0100

    añadir serialversionuid para evitar errores

--- src/agencia_prog_3_GUI/ExcursionPrecio.java
+++ src/agencia_prog_3_GUI/ExcursionPrecio.java
@@ -6,7 +6,7 @@ import javax.swing.SwingConstants;
 import javax.swing.table.DefaultTableCellRenderer;
 
 public class ExcursionPrecio extends DefaultTableCellRenderer{
-	
+	private static final long serialVersionUID = 1L;
 	public ExcursionPrecio() {
 		setHorizontalAlignment(SwingConstants.RIGHT);
 	}
