b855a5f1ae971a3840ffc7180f09bd95b558bcac
Laura Saez Serrano <laura.saez.serrano@opendeusto.es>
2025-11-26 11:39:08 +0100

    correcion de errores

--- src/agencia_prog_3_GUI/CuadriculaOfertas.java
+++ src/agencia_prog_3_GUI/CuadriculaOfertas.java
@@ -765,7 +765,7 @@ public class CuadriculaOfertas extends JFrame{
             String hab, int adultos, int ninos, Date salida, Date regreso, 
             double precioFinal) {
                 
-		final String FILE_NAME = "resosurces/data/reservas.csv";
+		final String FILE_NAME = "resources/data/reservas.csv";
 		// Formato de fecha estándar para CSV
 		final SimpleDateFormat SDF = new SimpleDateFormat("dd/MM/yyyy");
 
