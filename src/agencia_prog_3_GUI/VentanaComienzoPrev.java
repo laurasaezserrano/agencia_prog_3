380601bc4708c7a1c6e8aa0f9e96d6d720de9b71
Zaira Diez Garcia <zaira.diez@opendeusto.es>
2025-11-26 11:29:35 +0100

    eliminacion de metodos no utilizados

--- src/agencia_prog_3_GUI/VentanaComienzoPrev.java
+++ src/agencia_prog_3_GUI/VentanaComienzoPrev.java
@@ -1,9 +1,6 @@
 package agencia_prog_3_GUI;
 
-import java.awt.BorderLayout;
 import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.Image;
 import java.awt.event.ActionEvent;
@@ -16,10 +13,7 @@ import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JLayeredPane;
 import javax.swing.JOptionPane;
-import javax.swing.JPanel;
-import javax.swing.SwingConstants;
 import javax.swing.SwingUtilities;
 
 public class VentanaComienzoPrev extends JFrame{
@@ -66,7 +60,6 @@ public class VentanaComienzoPrev extends JFrame{
 	    boton.setBorderPainted(false);
 		    
 		    //Posicion X - Centrado Horizontalmente
-	    	int margen = 20;
 		    int botonX = 700;;
 		    //Posicion Y - Centrado Verticalmente
 		    int botonY = 200;;
